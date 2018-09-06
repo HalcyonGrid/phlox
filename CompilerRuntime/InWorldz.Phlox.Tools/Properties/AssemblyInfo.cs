14e245e47b142c804e3a57711c937f2dd454c3f7
Ricky C <kf6kjg@users.noreply.github.com>
2018-09-06 11:35:23 -0700

    Project and DLL rename from InWorldz to Halcyon

--- CompilerRuntime/InWorldz.Phlox.Tools/Properties/AssemblyInfo.cs
+++ CompilerRuntime/InWorldz.Phlox.Tools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InWorldz.Phlox.Tools")]
+[assembly: AssemblyTitle("Halcyon.Phlox.Tools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InWorldz.Phlox.Tools")]
+[assembly: AssemblyProduct("Halcyon.Phlox.Tools")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- CompilerRuntime/InWorldz.Phlox/Properties/AssemblyInfo.cs
+++ CompilerRuntime/InWorldz.Phlox/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InWorldz.Phlox")]
+[assembly: AssemblyTitle("Halcyon.Phlox")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InWorldz.Phlox")]
+[assembly: AssemblyProduct("Halcyon.Phlox")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
